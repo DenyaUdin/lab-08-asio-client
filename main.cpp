7c6f3394453d56213b8158e9ca60d2ec4813cbfc
DenyaUdin <47531190+DenyaUdin@users.noreply.github.com>
2020-01-06 16:45:19 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include <iostream>
 using namespace boost;
 using namespace std;
-void writeToSocket(asio::ip::tcp::socket& sock, std::string& buf) {  // Запись данных в сокет
+void writeToSocket(asio::ip::tcp::socket& sock, std::string& buf) {  
 	std::size_t total_bytes_written = 0;
 	while (total_bytes_written != buf.length()) {
 		total_bytes_written += sock.write_some(
@@ -16,67 +16,65 @@ void writeToSocket(asio::ip::tcp::socket& sock, std::string& buf) {  // Запи
 	}
 }
 
-bool flag = true; // Флаг работы клиента
+bool flag = true; 
 
-// Потоковая функция клиент
-void ClientThread(string clientName) // Параметр функции - имя клиента
+
+void ClientThread(string clientName) 
 {
-	std::string raw_ip_address = "127.0.0.1"; // Адрес сервера - локальеный компьютер (localhost)
-	unsigned short port_num = 3333; // Номер порта
-	asio::ip::tcp::endpoint ep(asio::ip::address::from_string(raw_ip_address),	port_num); // Создаем точку доступа
+	std::string raw_ip_address = "127.0.0.1"; 
+	unsigned short port_num = 3333;
+	asio::ip::tcp::endpoint ep(asio::ip::address::from_string(raw_ip_address),port_num); 
 	asio::io_service ios;
-	asio::ip::tcp::socket sock(ios, ep.protocol()); // Создаем сокет клиента
-	sock.connect(ep); // подключение сокета к точке доступа
+	asio::ip::tcp::socket sock(ios, ep.protocol()); 
+	sock.connect(ep); 
 
-	// Отправляем данные серверу
-	string strToServ = clientName;  // Первый раз отправляем имя клиент
-	cout << "To server: " << strToServ << endl; // Печать данных отправляемых на сервер
-	writeToSocket(sock, strToServ); // Отвправка - запись в сокет
-	// Получаем ответ сервера
-	boost::array<char, 256> buf; // Буфер для приема сообщения от сервера
-	boost::system::error_code error; // Суда записываем код ошибки (не используем)
-	size_t len = sock.read_some(boost::asio::buffer(buf), error); // Читаем данные от сервера
-	buf[len] = 0; // Строка заканчивается 0
-	cout  << "From server: " << buf.data(); // Печать данных от сервера
-	while (flag) // В цикле посылаем запрос, получаем ответ
+	
+	string strToServ = clientName;  
+	cout << "To server: " << strToServ << endl; 
+	writeToSocket(sock, strToServ); 
+	
+	boost::array<char, 256> buf; 
+	size_t len = sock.read_some(boost::asio::buffer(buf), error); 
+	buf[len] = 0; 
+	cout  << "From server: " << buf.data(); 
+	while (flag) 
 	{
-		switch (rand() % 3 + 1) // Случайно выбирает тип запроса
+		switch (rand() % 3 + 1) 
 		{
 		case 1:	strToServ = "ping\n"; break;
 		case 2:	strToServ = "clients\n"; break;
 		case 3:	strToServ = "Hernауауцya\n"; break;
 		}
-		cout << "To server: " << strToServ ; // Печать данных отправляемых на сервер
-		writeToSocket(sock, strToServ); // Отвправка - запись в сокет
+		cout << "To server: " << strToServ ; 
+		writeToSocket(sock, strToServ); 
 		// Получаем ответ сервера
-		size_t len = sock.read_some(boost::asio::buffer(buf), error); // Читаем данные от сервера
-		buf[len] = 0; // Строка заканчивается 0
-		cout << "From server: " << buf.data() ; // Печать данных от сервера
-		this_thread::sleep_for(chrono::seconds(1)); // Задержка 1 сек
+		size_t len = sock.read_some(boost::asio::buffer(buf), error); 
+		buf[len] = 0; 
+		cout << "From server: " << buf.data() ; 
+		this_thread::sleep_for(chrono::seconds(1)); 
 	}
 
-	// Завершение приема передачи, закрытие сокета
+	
 	sock.shutdown(asio::socket_base::shutdown_send);
-	// Close socket (is done by dtor)
 	sock.close();
 	
 }
 
-// Приложение клиент
+
 void main()
 {
 	
-	// Создаем потоки динамически
+	
 	cout << "For start thread, press Enter!\n";
 	cout << "For exit, press Enter!\n";
-	cin.get(); // Ждем нажатия Enter
+	cin.get(); 
 	thread t1(ClientThread, "MyClient145");
 	cout << "Thread1 Start Ok!!!!!\n";
-	this_thread::sleep_for(chrono::seconds(5)); // Задерка 5 сек
+	this_thread::sleep_for(chrono::seconds(5)); 
 	thread t2(ClientThread, "FghghhhMyClient");
 	cout << "Thread2 Start Ok!!!!!\n";
-	cin.get(); // Ждем нажатия Enter
-	flag = false; // завершение потоки
+	cin.get(); 
+	flag = false; 
 	t1.join();
 	t2.join();
 	cout << "Client is end!\n";
